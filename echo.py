04372b981f7cb34c5dd9503dc440aef1d4f967de
Miguel Ambrona <miguel.ambrona@imdea.org>
2018-10-10 14:24:06 +0200

    Fixing echo file

--- echo.py
+++ echo.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+import os
+import sys
+
+unbuffered = os.fdopen(sys.stdout.fileno(), 'w', 0)
+
 while True:
     a = raw_input()
-    print a
+    unbuffered.write(a + "\n")
